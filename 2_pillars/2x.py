a18f24f0c39bfa311ea0e1012032ce7c82a60d2f
PaszaVonPomiot <schedar@op.pl>
2023-01-27 00:47:51 +0100

    upd

---
+++ 2_pillars/2x.py
@@ -0,0 +1,11 @@
+"""
+Create Forest class with protected attribute indicating number of trees in it
+Add getter property retrieving number of trees
+Add setter property disallowing reducing number of trees
+Create instance of Forest
+Get number of trees and try changing number of trees to lower number
+"""
+
+
+class Forest:
+    ...
