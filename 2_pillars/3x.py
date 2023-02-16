b0fd954ca23b3bd2adbe89b703b41b93f70bc646
PaszaVonPomiot <schedar@op.pl>
2023-02-16 23:43:11 +0100

    upd

--- 2_pillars/3x.py
+++ 2_pillars/3x.py
@@ -2,6 +2,7 @@
 Create Image abstract class with instance attributes as parameters: width, length, bit_depth
 Image class defines abstract method export()
 Create Jpeg and Png derived classes that implement export() method
+export() returns string like "jpeg 800x600x24"
 """
 
 
