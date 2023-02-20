dcc03d22c005d61056b49caed86a8be7bbd56fc7
PaszaVonPomiot <schedar@op.pl>
2023-02-20 01:15:07 +0100

    upd

--- 2_pillars/1x.py
+++ 2_pillars/1x.py
@@ -2,17 +2,18 @@
 SINGLE INHERITANCE
 
 Define 2 classes - Alcohol & Beer
-Alcohol has method drink()
-Beer has method drink_fast()
+Alcohol has method drink() doing print("Gulp!")
+Beer has method drink_fast() doing print("Gulp, Gulp, Gulp!")
 Make an instance of Beer and call drink()
 """
+
 
 """
 MULTIPLE INHERITANCE
 
 Define 3 classes - Car, Electric, Skoda
 Skoda inherits from Car & Electric
-Instance of Skoda have access to Car attribute and Electric method
+Instance of Skoda have access to Car attribute and Electric method (both without implementation)
 Make an instance of Skoda and call its method
 """
 
