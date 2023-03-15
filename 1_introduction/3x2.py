acb26255f039756d09c96229290602a09087869a
PaszaVonPomiot <schedar@op.pl>
2023-03-15 22:35:12 +0100

    upd

--- 1_introduction/3x2.py
+++ 1_introduction/3x2.py
@@ -8,7 +8,7 @@ class Car:
 
 
 car = Car(brand="Skoda", color="red")
-car.print_mileage()  # should be 0
+car.print_mileage()  # should be 0 initially
 car.drive(kilometers=100)
 
 print(f"My {car.brand} is {car.color} and drove for {car.mileage}")
--- 2_pillars/1x.py
+++ 2_pillars/1x.py
@@ -20,8 +20,11 @@ Make an instance of Skoda and call its method
 """
 INHERITANCE & COMPOSITION
 
-Define Head and Torso classes derived from BodyParts
-Head implements talk() method
-Define Human class composed of Head and Torso
-Make an instance of Human and call talk() method
+Define BodyPart class that has attribute self.hitpoints=100
+Define Head, Torso and Hand classes derived from BodyPart
+Head implements talk() method that prints "Hello"
+Hand implements slap() method that print "Slap"
+
+Define Human class composed of head, torso, left_hand and right_hand
+Make an instance of Human and make it talk() and slap() with right_hand
 """
